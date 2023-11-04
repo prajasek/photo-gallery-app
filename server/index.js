e0c49441d7e2628342f70841b009293ab4319bd7
Prasanth <sekaranp92@gmail.com>
2023-11-03 21:49:52 -0400

    test mongoose connection to localhost

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb"}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/school").then(() => {
+    let x = mongoose.model('student', new mongoose.Schema({}));
+    console.log(x);
+
+    let y;
+    let m = x.find().then(x=> console.log(x));
+
+})
