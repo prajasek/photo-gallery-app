b2bcdf6fd2ef85ed62c6e5aec98525929a8eaa9f
Prasanth <sekaranp92@gmail.com>
2023-11-21 07:34:05 -0500

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import Test from "./test";
 
 const App = () => {
   return (
     <div>
          <h1> App </h1>
+         <Test />
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,18 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import 'dotenv/config';
 
+console.log(process.env.x)
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb"}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/school").then(() => {
-    let x = mongoose.model('student', new mongoose.Schema({}));
-    console.log(x);
-
-    let y;
-    let m = x.find().then(x=> console.log(x));
-
+mongoose.connect("mongodb+srv://prasanthdps:@picturebook.ca6dws8.mongodb.net/").then(() => {
+  
+    console.log()
 })
